d481b5b8f5dcea5d466744f69062f424b5b4fd43
Ruth Kasavo <u15288057@tuks.co.za>
2026-04-23 10:06:33 +0200

    updated fact table to use inner join instead of left join

--- sql_scripts/5. staging_shop_dim.sql
+++ sql_scripts/5. staging_shop_dim.sql
@@ -1,12 +1,11 @@
 --create if not exist
-DROP TABLE computer_std.dbo.dim_shop;
+IF OBJECT_ID('computer_std.dbo.dim_shop', 'U') IS NOT NULL
+    DROP TABLE computer_std.dbo.dim_shop;
 CREATE TABLE computer_std.dbo.dim_shop (
     Shop_ID INT IDENTITY(1,1) PRIMARY KEY,
     [Shop Name] VARCHAR(250) NULL,
     [Shop Age] INT NULL
 );
-
-
 --insert data into the table
 INSERT INTO computer_std.dbo.dim_shop 
 	([Shop Name], [Shop Age])
@@ -16,16 +15,3 @@ FROM computer_std.dbo.pc_data;
 
 --select all data from the dim table
 SELECT * FROM computer_std.dbo.dim_shop;
-
-WITH ShopCleaner AS (
-    SELECT 
-        Shop_ID, 
-        [Shop Name],
-        ROW_NUMBER() OVER (
-            PARTITION BY [Shop Name] 
-            ORDER BY Shop_ID
-        ) AS RowNum
-    FROM computer_std.dbo.dim_shop
-)
-DELETE FROM ShopCleaner 
-WHERE RowNum > 1;
--- sql_scripts/9. staging_fact_table.sql
+++ sql_scripts/9. staging_fact_table.sql
@@ -1,4 +1,3 @@
-
 IF OBJECT_ID('computer_std.dbo.Fact_PC_Sales', 'U') IS NOT NULL
     DROP TABLE computer_std.dbo.Fact_PC_Sales;
 
@@ -21,140 +20,71 @@ CREATE TABLE computer_std.dbo.Fact_PC_Sales (
     [PC Market Price] DECIMAL(18,2)
 );
 
-WITH dim_customer_clean AS (
-    SELECT *,
-           ROW_NUMBER() OVER (
-               PARTITION BY [Customer Name], [Customer Surname]
-               ORDER BY Customer_ID
-           ) AS rn
-    FROM computer_std.dbo.dim_customer
-),
-dim_sales_person_clean AS (
-    SELECT *,
-           ROW_NUMBER() OVER (
-               PARTITION BY [Sales Person Name], [Sales Person Department]
-               ORDER BY Sales_Person_ID
-           ) AS rn
-    FROM computer_std.dbo.dim_sales_person
-),
-dim_product_clean AS (
-    SELECT *,
-           ROW_NUMBER() OVER (
-               PARTITION BY [PC Model], [PC Make]
-               ORDER BY Product_ID
-           ) AS rn
-    FROM computer_std.dbo.dim_product
-),
-dim_location_clean AS (
-    SELECT *,
-           ROW_NUMBER() OVER (
-               PARTITION BY Continent, [Country or State]
-               ORDER BY Location_ID
-           ) AS rn
-    FROM computer_std.dbo.dim_location
-),
-dim_shop_clean AS (
-    SELECT *,
-           ROW_NUMBER() OVER (
-               PARTITION BY [Shop Name]
-               ORDER BY Shop_ID
-           ) AS rn
-    FROM computer_std.dbo.dim_shop
-),
-dim_channel_clean AS (
-    SELECT *,
-           ROW_NUMBER() OVER (
-               PARTITION BY Channel
-               ORDER BY Channel_ID
-           ) AS rn
-    FROM computer_std.dbo.dim_channel
-),
-dim_payment_clean AS (
-    SELECT *,
-           ROW_NUMBER() OVER (
-               PARTITION BY [Payment Method]
-               ORDER BY Payment_Method_ID
-           ) AS rn
-    FROM computer_std.dbo.dim_payment_method
-),
-dim_priority_clean AS (
-    SELECT *,
-           ROW_NUMBER() OVER (
-               PARTITION BY Priority
-               ORDER BY Priority_ID
-           ) AS rn
-    FROM computer_std.dbo.dim_priority
-),
-source_dedup AS (
-    SELECT *,
-           ROW_NUMBER() OVER (
-               PARTITION BY 
-                   [Customer Name], [Customer Surname],
-                   [Sales Person Name], [Sales Person Department],
-                   [PC Model], [PC Make],
-                   Continent, [Country or State],
-                   [Shop Name], Channel,
-                   [Payment Method], Priority,
-                   [Purchase Date], [Sale Price]
-               ORDER BY [Purchase Date]
-           ) AS rn
-    FROM computer_std.dbo.pc_data
-)
-
 INSERT INTO computer_std.dbo.Fact_PC_Sales (
-    Customer_ID, Sales_Person_ID, Product_ID, Location_ID, 
-    Channel_ID, Shop_ID, Payment_Method_ID, Priority_ID, 
-    [Cost Price], [Sale Price], [Discount Amount], 
-    [Purchase Date], [Ship Date], [Credit Score], 
+    Customer_ID, Sales_Person_ID, Product_ID, Location_ID,
+    Channel_ID, Shop_ID, Payment_Method_ID, Priority_ID,
+    [Cost Price], [Sale Price], [Discount Amount],
+    [Purchase Date], [Ship Date], [Credit Score],
     [Cost of Repairs], [PC Market Price]
 )
+
 SELECT 
     cust.Customer_ID,
     sp.Sales_Person_ID,
     prod.Product_ID,
     loc.Location_ID,
     chan.Channel_ID,
-    ds.Shop_ID,
+    shop.Shop_ID,
     pay.Payment_Method_ID,
     prio.Priority_ID,
-    pd.[Cost Price],
-    pd.[Sale Price],
-    pd.[Discount Amount],
-    TRY_CAST(pd.[Purchase Date] AS DATE),
-    TRY_CAST(pd.[Ship Date] AS DATE),
-    pd.[Credit Score],
-    pd.[Cost of Repairs],
-    pd.[PC Market Price]
-FROM source_dedup pd
-LEFT JOIN dim_customer_clean cust 
-    ON pd.[Customer Name] = cust.[Customer Name]
-   AND pd.[Customer Surname] = cust.[Customer Surname]
-   AND cust.rn = 1
-LEFT JOIN dim_sales_person_clean sp 
-    ON pd.[Sales Person Name] = sp.[Sales Person Name]
-   AND pd.[Sales Person Department] = sp.[Sales Person Department]
-   AND sp.rn = 1
-LEFT JOIN dim_product_clean prod 
-    ON pd.[PC Model] = prod.[PC Model]
-   AND pd.[PC Make] = prod.[PC Make]
-   AND prod.rn = 1
-LEFT JOIN dim_location_clean loc 
-    ON pd.Continent = loc.Continent
-   AND pd.[Country or State] = loc.[Country or State]
-   AND loc.rn = 1
-LEFT JOIN dim_shop_clean ds 
-    ON pd.[Shop Name] = ds.[Shop Name]
-   AND ds.rn = 1
-LEFT JOIN dim_channel_clean chan 
-    ON pd.Channel = chan.Channel
-   AND chan.rn = 1
-LEFT JOIN dim_payment_clean pay 
-    ON pd.[Payment Method] = pay.[Payment Method]
-   AND pay.rn = 1
-LEFT JOIN dim_priority_clean prio 
-    ON pd.Priority = prio.Priority
-   AND prio.rn = 1
-WHERE pd.rn = 1;
+    pc.[Cost Price],
+    pc.[Sale Price],
+    pc.[Discount Amount],
+    TRY_CAST(pc.[Purchase Date] AS DATE),
+    TRY_CAST(pc.[Ship Date] AS DATE),
+    pc.[Credit Score],
+    pc.[Cost of Repairs],
+    pc.[PC Market Price]
+
+FROM computer_std.dbo.pc_data pc
+
+INNER JOIN computer_std.dbo.dim_customer cust
+    ON pc.[Customer Name] = cust.[Customer Name]
+   AND pc.[Customer Surname] = cust.[Customer Surname]
+   AND pc.[Customer Contact Number]= cust.[Customer Contact Number]
+   AND pc.[Customer Email Address] = cust.[Customer Email Address]
+
+INNER JOIN computer_std.dbo.dim_sales_person sp
+    ON pc.[Sales Person Name] = sp.[Sales Person Name]
+   AND pc.[Sales Person Department] = sp.[Sales Person Department]
+
+INNER JOIN computer_std.dbo.dim_product prod
+    ON pc.[PC Model] = prod.[PC Model]
+   AND pc.[PC Make] = prod.[PC Make]
+   AND pc.[Storage Capacity] = prod.[Storage Capacity]
+   AND pc.[RAM] = prod.[RAM]
+   AND pc.[Storage Type] = prod.[Storage Type]
+
+INNER JOIN computer_std.dbo.dim_location loc
+    ON pc.Continent = loc.Continent
+   AND pc.[Country or State] = loc.[Country or State]
+   AND pc.[Province or City] = loc.[Province or City]
+
+INNER JOIN computer_std.dbo.dim_shop shop
+    ON pc.[Shop Name] = shop.[Shop Name]
+    AND pc.[Shop Age] = shop.[Shop Age]
+
+INNER JOIN computer_std.dbo.dim_channel chan
+    ON pc.Channel = chan.Channel
+
+INNER JOIN computer_std.dbo.dim_payment_method pay
+    ON pc.[Payment Method] = pay.[Payment Method]
+
+INNER JOIN computer_std.dbo.dim_priority prio
+    ON pc.Priority = prio.Priority;
+
+
 
 SELECT (SELECT COUNT(*) FROM computer_std.dbo.Fact_PC_Sales) AS FactCount,
        (SELECT COUNT(*) FROM computer_std.dbo.pc_data) AS RawCount;
+
